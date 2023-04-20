ec125bf2927f877184987abbf9176c0f66128b18
Ricardo Lamar <ricardo.lamar@gmail.com>
2023-04-20 16:48:23 -0400

    Parte 1 de 10. Server Side

--- aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Acme.BookStore.Books;
+using AutoMapper;
 
 namespace Acme.BookStore;
 
@@ -6,8 +7,8 @@ public class BookStoreApplicationAutoMapperProfile : Profile
 {
     public BookStoreApplicationAutoMapperProfile()
     {
-        /* You can configure your AutoMapper mapping configuration here.
-         * Alternatively, you can split your mapping configurations
-         * into multiple profile classes for a better organization. */
+        // You can configure your AutoMapper mapping configuration here.
+        CreateMap<Book, BookDto>();
+        CreateMap<CreateUpdateBookDto, Book>();
     }
 }
