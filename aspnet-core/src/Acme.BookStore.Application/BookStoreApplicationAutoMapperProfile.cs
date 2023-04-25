5cdba5a1886c088afec75ba8a7f0723a458bfec4
rirelam <ricardo.lamar@gmail.com>
2023-04-24 22:06:14 -0400

    Parte 8 de 10: por terminar faltan pruebas

--- aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ aspnet-core/src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using Acme.BookStore.Books;
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
 using AutoMapper;
 
 namespace Acme.BookStore;
@@ -10,5 +11,8 @@ public class BookStoreApplicationAutoMapperProfile : Profile
         // You can configure your AutoMapper mapping configuration here.
         CreateMap<Book, BookDto>();
         CreateMap<CreateUpdateBookDto, Book>();
+
+        CreateMap<Author, AuthorDto>();
+
     }
 }
